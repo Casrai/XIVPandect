28d85e2d197495c1ea6b2aab64685dff99088158
Cas <101531793+Casrai@users.noreply.github.com>
2023-11-12 17:51:51 +0100

    darkmode?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let name = 'World';
+  </script>
+  
+  <style>
+    main {
+      max-width: 600px;
+      margin: 50px auto;
+      text-align: center;
+    }
+  
+    h1 {
+      color: #ff3e00;
+    }
+  
+    input {
+      margin-top: 10px;
+      padding: 5px;
+    }
+  </style>
+  
+  <main>
+    <h1>Hello {name}!</h1>
+    <p>This is a Svelte app. Edit the code in <code>src/Home.svelte</code> and save to reload.</p>
+  
+    <label for="nameInput">Enter your name:</label>
+    <input type="text" id="nameInput" bind:value={name} />
+  </main>
+  
